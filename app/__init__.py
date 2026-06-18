0213d04b1340674fbdcda3eac2090e8ab111a207
Aayushakarki177-Star <aayushakarki177@gmail.com>
2026-06-18 16:55:24 +0545

    app: register profile routes in create_app

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,15 @@
 from flask import Flask
 import config
-
 from app.models.database import Database
 from app.routes.authroutes import AuthRoutes
 
 def create_app():
-
     app = Flask(__name__)
     app.secret_key = config.SECRET_KEY
 
-    # create tables automatically
     with app.app_context():
         Database.create_tables()
 
-    # register routes
     auth_routes = AuthRoutes()
     app.register_blueprint(auth_routes.register())
 

879703e9dbe8a626b7ffe827ee5834582fabf49a
Aayushakarki177-Star <aayushakarki177@gmail.com>
2026-06-18 16:54:24 +0545

    config: add database secret key and MySQL credentials

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ SECRET_KEY = 'random-secret-key'
 # -----------------------
 MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'root@2'
+MYSQL_PASSWORD = 'aayusha8955@'
 MYSQL_DATABASE = 'meropasal'

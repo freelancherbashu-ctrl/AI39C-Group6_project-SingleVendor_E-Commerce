453c74b651f0c41f008b8773e5c16f3097d488c5
Prasis64 <prasisp2008@gmail.com>
2026-06-19 21:02:00 +0545

    app/extensions.py added

--- app/extensions.py
+++ app/extensions.py
@@ -3,6 +3,8 @@ pymysql.install_as_MySQLdb()
 
 from flask_mysqldb import MySQL
 from flask_mail import Mail
+from flask_wtf.csrf import CSRFProtect
 
 mysql = MySQL()
 mail  = Mail()
+csrf  = CSRFProtect()
